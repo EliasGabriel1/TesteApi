ef7a8ed05456b548e514c4fa353972c1c47bcbbd
EliasGabriel1 <elias.147682@hotmail.com>
2021-11-02 18:42:01 -0300

    update

--- src/data/data.js
+++ src/data/data.js
@@ -1,17 +1,184 @@
 const dados = [
   {
-    Receita: "MOUSSE DE LIMÃO",
-    INGREDIENTES:"1 lata de leite condensado 1, lata de creme de leite1/2, xícara de suco de limão (esse suco é puro mesmo, sem água, é só espremer o limão)",
-    hdurl: "https://www.anamariabrogui.com.br/assets/uploads/receitas/fotos/usuario-2945-0f816c385bfc6425c3e550cc4e5adfac.jpg",
-    PREPARO: "Coloque no liquidificador o creme de leite (com soro mesmo) e o leite condensado. Bata um pouco e depois vá acrescentando o suco do limão, aos poucos. Ele vai ficar bem consistente, leve à geladeira.",
-    obs: "rapido de fazer",
-  },  {
-    Receita: "MOUSSE DE LIMÃO",
-    INGREDIENTES:"1 lata de leite condensado 1, lata de creme de leite1/2, xícara de suco de limão (esse suco é puro mesmo, sem água, é só espremer o limão)",
-    hdurl: "https://www.anamariabrogui.com.br/assets/uploads/receitas/fotos/usuario-2945-0f816c385bfc6425c3e550cc4e5adfac.jpg",
-    PREPARO: "Coloque no liquidificador o creme de leite (com soro mesmo) e o leite condensado. Bata um pouco e depois vá acrescentando o suco do limão, aos poucos. Ele vai ficar bem consistente, leve à geladeira.",
-    obs: "rapido de fazer",
-  }
+    id: 1,
+    title: "Ana",
+    profissao: "Especialista Em Marketing Performance",
+    img: "https://cdn.pixabay.com/photo/2017/02/16/12/18/business-woman-2071349_960_720.jpg",
+    cores: ["#ffc0cb", "#fff", "#000"],
+    side1: [
+      {
+        about: "Salario:",
+        valor: "R$ 7.580",
+      },
+      {
+        about: "Onde:",
+        valor: "pipefaio",
+      },
+      {
+        about: "Características da profissão:",
+        valor: "Olhar analítico e crítico que busca entender pessoas",
+      },
+    ],
+    side2: [
+      {
+        descricao: "Características:",
+        valor: "Olhar critico de análitico",
+      },
+      {
+        descricao: "Qual livro e filme preferido sucessivamente?",
+        valor: "Harry potter - carros",
+      },
+      {
+        descricao: "Descrição:",
+        valor:
+          "Gosto de gatos, amo ler, gosto de chá e meu hobie é ver revista de modas",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Michel",
+    profissao: "Professor de matemática",
+    img: "https://cdn.pixabay.com/photo/2019/06/29/04/36/counselor-4305394_960_720.jpg",
+    cores: ["#ffff00", "#ffa500", "#000"],
+    side1: [
+      {
+        about: "Salário:",
+        valor: "R$ 2.400",
+      },
+      {
+        about: "Onde:",
+        valor: "math .io",
+      },
+      {
+        about: "Características da profissão:",
+        valor: "Olhar matemático, rígido, e que busca compreender problemas e resolver questão com a lógica",
+      },
+    ],
+    side2: [
+      {
+        descricao: "Características:",
+        valor: "Perfil analítico com olhar estratégico",
+      },
+      {
+        descricao: "Qual livro e filme preferido sucessivamente?",
+        valor: "Não gosto de ler, e o filme preferido é Indiana Jones",
+      },
+      {
+        descricao: "Descrição:",
+        valor:
+          "Gostar de kamban, jogar League of Legends e jogos estratégicos, O cotidiano é passado de acordar, beber café, e sair para o serviço sempre chegar em ponto, no almoço, come coisas simples no almoço, café da tarde gosta de tomar café, tem preferência em dar aulas por alunos de 17 anos+, pois o resultado é muito maior, anda de bicicleta, e como lugar preferido pra comer, uma pizzaria com clima razoavelmente suave, com conexão com poucos amigos, com quem tem mais intimidades. ",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Anne",
+    profissao: "Bióloga",
+    img: "https://cdn.pixabay.com/photo/2015/09/02/13/24/girl-919048_960_720.jpg",
+    cores: ["#008", "#4169e1", "#f8f8f8"],
+    side1: [
+      {
+        about: "Salario:",
+        valor: "R$ 4.058",
+      },
+      {
+        about: "Onde:",
+        valor: "Empresa Bionat",
+      },
+      {
+        about: "Características da profissão:",
+        valor: "olhar suave, gosta do natural, e sente íntimo de tudo que relaciona a natureza",
+      },
+    ],
+    side2: [
+      {
+        descricao: "Características:",
+        valor: "Perfil tranquilo, com conexão com a natureza",
+      },
+      {
+        descricao: "Qual livro e filme preferido sucessivamente?",
+        valor: "livro seleção, e gosta da saga crepúsculo",
+      },
+      {
+        descricao: "Descrição:",
+        valor:
+          "Gosta de fazer viagens que envolve a natureza, ama animais, e luta pelos direitos da vida dos animais. Gosta de sair com os amigos, curtir a vida, segue conceitos hippie, gosta de músicas popular nacional, no seu cotidiano sempre gosta de pegar Starbucks antes de ir pro serviço, trabalha na linha de pesquisa, com acesso diários em fóruns e google acadêmico, gosta de publicar nas redes sociais suas viagens, aonde se conecta com a natureza e se desliga do mundo, e como melhor amigo tem um gato e uma cobra.",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Rodrigo",
+    profissao: "Diretor de Arte",
+    img: "https://cdn.pixabay.com/photo/2016/11/21/12/42/beard-1845166_960_720.jpg",
+    cores: ["#008", "#c60ef8", "#000"],
+    side1: [
+      {
+        about: "Salario:",
+        valor: "R$ 7.580",
+      },
+      {
+        about: "Onde:",
+        valor: "pipefaio",
+      },
+      {
+        about: "Características da profissão:",
+        valor: "Olhar analítico e crítico que busca entender pessoas",
+      },
+    ],
+    side2: [
+      {
+        descricao: "Características:",
+        valor: "Olhar criativo e descontraido",
+      },
+      {
+        descricao: "Qual livro e filme preferido sucessivamente?",
+        valor: "Harry potter - carros",
+      },
+      {
+        descricao: "Descrição:",
+        valor:
+          "Gosto de Star Wars, ama desenhar e gosta de pinturas",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Camila",
+    profissao: "Odontologia",
+    img: "https://image.freepik.com/fotos-gratis/dentista-feminina-com-ferramentas-de-odontologia-isolado_1303-13189.jpg",
+    cores: ["#ffc0cb", "#fff", "#000"],
+    side1: [
+      {
+        about: "Salario:",
+        valor: "R$ 7.580",
+      },
+      {
+        about: "Onde:",
+        valor: "Onodontoclick",
+      },
+      {
+        about: "Características da profissão:",
+        valor: "Olhar seguro e cientificamente análitico",
+      },
+    ],
+    side2: [
+      {
+        descricao: "Características:",
+        valor: "Perfil descontraido porém desconfiado, bem responsável e séria",
+      },
+      {
+        descricao: "Qual livro e filme preferido sucessivamente?",
+        valor: "Pequeno Principe - Capitão América: Guerra Civil",
+      },
+      {
+        descricao: "Descrição:",
+        valor:
+          "Gosta de sair para lugares mais suaves como shopping, restaurante e pizzaria, apesar de não possuir tempo, gosta de ler, gosta de redes sociais, porém passa pouco tempo nelas, passa a maior parte do tempo estudante e quando tem tempo gosta de sair com sua filha, maior desafio diário, é ser mãe sozinha.",
+      },
+    ],
+  },
 ];
 
 module.exports = dados;
